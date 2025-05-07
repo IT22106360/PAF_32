b17b8300aea5f66ba50dccc086dad29373c94481
Rashmika Rupasinghe <127801884+IT22106360@users.noreply.github.com>
2025-05-07 21:25:35 +0530

    Update MessagingController.java

--- Back-end/src/main/java/EduConnect/LearningApp/backend/features/messaging/controller/MessagingController.java
+++ Back-end/src/main/java/EduConnect/LearningApp/backend/features/messaging/controller/MessagingController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/messaging")
+@RequestMapping("/api/v1/messag")
 public class MessagingController {
     private final MessagingService messagingService;
 
