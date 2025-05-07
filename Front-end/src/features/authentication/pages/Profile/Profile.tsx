59a32590a310ca7e43cedf1afd5c9e8519e68f8d
Rashmika Rupasinghe <127801884+IT22106360@users.noreply.github.com>
2025-05-07 21:17:51 +0530

    Update Profile.tsx

--- Front-end/src/features/authentication/pages/Profile/Profile.tsx
+++ Front-end/src/features/authentication/pages/Profile/Profile.tsx
@@ -20,7 +20,7 @@ export function Profile() {
   });
   const onSubmit = async () => {
     if (!data.firstName || !data.lastName) {
-      setError("Please fill in your first and last name.");
+      setError("Please fill in your first and l name.");
       return;
     }
     if (!data.company || !data.position) {
